e9bb52610466b64e8be44de0d640567b7608ace5
Topper Bowers <topper@illusionoflife.inc>
2025-12-15 14:41:09 +0100

    allow setting URLs

--- packages/react/src/components/SoulEngineProvider.tsx
+++ packages/react/src/components/SoulEngineProvider.tsx
@@ -3,16 +3,25 @@ import { SoulsProvider } from './SoulsProvider';
 import { SharedContextProvider } from './SharedContextProvider';
 import { useIsClient } from '../hooks/useIsClient';
 import { SharedContextToken } from '../lib/SharedContext';
+import type { GetWebSocketUrl } from "./SoulsProvider";
 
 interface SoulEngineProviderProps {
   organization: string;
   sharedContextToken?: SharedContextToken;
   sharedContextTokens?: Record<string, SharedContextToken>;
   local?: boolean;
+  getWebSocketUrl?: GetWebSocketUrl;
   children: React.ReactNode;
 }
 
-export const SoulEngineProvider: React.FC<SoulEngineProviderProps> = ({ organization, sharedContextToken, sharedContextTokens, children, local }) => {
+export const SoulEngineProvider: React.FC<SoulEngineProviderProps> = ({
+  organization,
+  sharedContextToken,
+  sharedContextTokens,
+  children,
+  local,
+  getWebSocketUrl,
+}) => {
   const isClient = useIsClient()
 
   if (!isClient) {
@@ -23,7 +32,7 @@ export const SoulEngineProvider: React.FC<SoulEngineProviderProps> = ({ organiza
   
   return (
     <SharedContextProvider organization={organization} token={sharedContextToken} tokens={sharedContextTokens} local={local}>
-      <SoulsProvider organization={organization}>
+      <SoulsProvider organization={organization} getWebSocketUrl={getWebSocketUrl}>
         {children}
       </SoulsProvider>
     </SharedContextProvider>
