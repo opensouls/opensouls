09977b76da0fe52479df49cf93658b0d5cf11db0
Topper Bowers <topper@illusionoflife.inc>
2025-12-16 08:49:20 +0100

    document ephemeral event

--- packages/beta-docs/pages/souls/_meta.ts
+++ packages/beta-docs/pages/souls/_meta.ts
@@ -2,6 +2,7 @@ const meta = {
   'processing-cycle': 'Processing Cycle',
   debugging: 'Debugging',
   perceptionAndInteractionRequest: 'Perceptions & InteractionRequests',
+  'ephemeral-events': 'Ephemeral Events',
   tools: 'Tools',
   'jwt-authentication': 'JWT Authentication',
 };
